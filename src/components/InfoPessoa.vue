c8e8ac067dde190b1e63e7aa317cf6e705a51448
Jean Machado <jeanmachadobrasil@gmail.com>
2026-02-24 18:47:03 -0300

    Refactor component structure: remove unused Pessoa and Info components, and create InfoPessoa component

--- src/components/InfoPessoa.vue
+++ src/components/InfoPessoa.vue
@@ -1,6 +1,9 @@
 <template>
-    <p>
+    <p v-if="esta_trabalhando">
         Estou trabalhando no momento.
+    </p>
+    <p v-else>
+        Estou em busca de emprego no momento.
     </p>
     <p>
         Utilizo as seguintes tecnologias:
@@ -10,11 +13,25 @@
         <li>JavaScript</li>
         <li>HTML</li>
     </ul>
+    <p v-show="mostrar_email">
+        Mande uma mensagem para : {{ email }}
+    </p>
+    <p>
+        Para acessar meu site, basta clicar no link abaixo:
+        <a v-bind:href="link" target="_blank">Meu Site</a>
+    </p>
 </template>
 
 <script>
     export default {
         name: 'InfoPessoa',
-
+        data(){
+            return{
+                mostrar_email: true,
+                email: 'jeanmachadobrasil@gmail.com',
+                esta_trabalhando: false,
+                link: 'https://github.com/jeanmbr'
+            }
+        }
     }
 </script>
