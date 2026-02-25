a8d41527c9116a92c68d7856bf8ec83370d4a3e3
Jean Machado <jeanmachadobrasil@gmail.com>
2026-02-25 18:03:14 -0300

    Implement structure for code changes with placeholders for future updates

---
+++ src/components/FotoGrafia.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="avatar" :alt="descricao">
+</template>
+
+<script>
+    export default{
+        name: 'FotoGrafia',
+        data(){
+            return{
+                avatar: "/img/java.png",
+                descricao: "Logo do Java"
+            }
+        }
+    }
+</script>
